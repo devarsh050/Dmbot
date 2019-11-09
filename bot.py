bc57aef711992b6da484323b438183680f53378a
devarsh050 <55909448+devarsh050@users.noreply.github.com>
2019-11-09 22:03:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ from discord.ext import commands
 import random
 import aiohttp
 import json
-Import time
+import time
 
 if BotToken == "Enter your Bot Token here!":
     raise ValueError("Please enter your Bot Token in line 1")
